3cb1ed0f829d430db1b4f7a64684858e700e8884
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-03 09:09:49 +0900

    Initial commit

---
+++ PowerfulSwiftUI/PowerfulSwiftUI/PowerfulSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerfulSwiftUIApp.swift
+//  PowerfulSwiftUI
+//
+//  Created by NoelMacMini on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PowerfulSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
