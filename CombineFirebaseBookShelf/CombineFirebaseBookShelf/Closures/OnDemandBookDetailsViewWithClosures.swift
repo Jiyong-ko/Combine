c732295e07dea3a80eb98dacdfae26977484612a
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-09 13:32:25 +0900

    온디맨드 북 세부정보 뷰 - 컴바인 타입 실습: Firestore에서 책 정보를 가져오고 오류 메시지 표시 기능 추가 및 UI 업데이트

--- CombineFirebaseBookShelf/CombineFirebaseBookShelf/Closures/OnDemandBookDetailsViewWithClosures.swift
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/Closures/OnDemandBookDetailsViewWithClosures.swift
@@ -6,13 +6,65 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+
+private class BookDetailViewModel: ObservableObject {
+  @Published var book = Book.empty
+  @Published var errorMessage: String?
+  
+  private var db = Firestore.firestore()
+  
+  let documentIds = ["hitchhiker", "MeODzVIGPx9diQlYgZHG", "HtHSzOdqjX3CREtwR3nD"]
+  
+  fileprivate func fetchBook() {
+    let randomId = documentIds.randomElement() ?? "hitchhiker"
+    // Firestore의 documentReference를 사용하여
+    let docRef = db.collection("books").document(randomId)
+    
+    // Firestore의 document를 가져온다.
+    docRef.getDocument(as: Book.self) { [weak self] result in
+      switch result {
+      case .success(let book):
+        self?.book = book
+        self?.errorMessage = nil
+      case .failure(let error):
+        self?.book = Book.empty
+        self?.errorMessage = error.localizedDescription
+      }
+    }
+  }
+}
 
 struct OnDemandBookDetailsViewWithClosures: View {
+  @StateObject private var viewModel = BookDetailViewModel()
+  
     var body: some View {
-      VStack {
-        EmptyView()
+      Form {
+         Section {
+           Text(viewModel.book.title)
+             .font(.title)
+             .padding()
+           Text(viewModel.book.author)
+             .font(.headline)
+             .padding()
+           Text("\(viewModel.book.numberOfPages) pages")
+             .font(.subheadline)
+             .padding()
+         } footer: {
+           if let errorMessage = viewModel.errorMessage {
+             Text(errorMessage)
+               .foregroundColor(.red)
+               .padding()
+           }
+         }
       }
       .navigationTitle("Book Details")
+      .onAppear {
+          viewModel.fetchBook()
+        }
+        .refreshable {
+          viewModel.fetchBook()
+        }
     }
 }
 
