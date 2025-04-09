f070ef6ec378fcc8ae608348a4e08fa55adbcdf6
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-09 09:40:14 +0900

    CombineFirebaseBookShelfApp 프로젝트 추가

---
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/CombineFirebaseBookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFirebaseBookShelfApp.swift
+//  CombineFirebaseBookShelf
+//
+//  Created by NoelMacMini on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineFirebaseBookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
